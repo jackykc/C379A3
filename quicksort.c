cc652f096763de1b4fdcb78abbef7aea21ddd945
Andrei <drei3000@gmail.com>
2016-11-21 21:32:41 -0700

    Quicksort works

--- quicksort.c
+++ quicksort.c
@@ -7,35 +7,42 @@ int midPoint(int left, int right) {
 	return (left+right)/2;
 }
 
-int quickSort(int array[], int left, int right) {
-	
+void quickSort(int array[], int left, int right) {
+	int tempLeft = left;
+	int tempRight = right;
+	int temp;
+	int pivot = round(midPoint(left, right));
+	while (tempLeft <= tempRight) {
+		while (array[tempLeft] < array[pivot]) {
+			tempLeft++;
+		}
+		while (array[tempRight] > array[pivot]) {
+			tempRight--;
+		}
+		if (tempLeft <= tempRight) {
+			temp = array[tempLeft];
+			array[tempLeft] = array[tempRight];
+			array[tempRight] = temp;
+			tempLeft++;
+			tempRight--;
+		}
+	}
+	if (left < tempRight) {
+		quickSort(array, left, tempRight);
+	}
+	if (tempLeft < right) {
+		quickSort(array, tempLeft, right);
+	}
 }
 
 int main(int argc, char *argv[]) {
-	int i, temp;
-	int myArray[7] = {3,4,9,6,2,5,7};
+	int i;
+	int myArray[] = {3,4,9,6,2,5,7};
 	for (i = 0; i<(sizeof(myArray)/sizeof(int));i++) {
 		printf("%i ", myArray[i]);
 	}
 	printf("\n");
-	int left = 0;
-	int right = sizeof(myArray)/sizeof(int)-1;
-	int pivot = round(midPoint(0, sizeof(myArray)/sizeof(int)));
-	while (left <= right) {
-		while (myArray[left] < myArray[pivot]) {
-			left++;
-		}
-		while (myArray[right] > pivot) {
-			right--;
-		}
-		if (left <= right) {
-			temp = myArray[left];
-			myArray[left] = myArray[right];
-			myArray[right] = temp;
-			left++;
-			right--;
-		}
-	}
+	quickSort(myArray, 0, 6);
 
 	// while (pivot != sizeof(myArray)/sizeof(int)) {}
 	for (i = 0; i<(sizeof(myArray)/sizeof(int));i++) {
