a6918a3e2b7f75d051dc7990640d12393eab0612
Andrei <drei3000@gmail.com>
2016-11-21 20:38:26 -0700

    Working on Quicksort

--- quicksort.c
+++ quicksort.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+#include <string.h>
+// Quicksort function.
 int main(int argc, char *argv[]) {
+	int i;
+	int myArray[8] = {3,7,4,9,10,2,5,7};
 
-	printf("This is quicksort\n");
 
+	for (i = 0; i<sizeof(myArray);i++) {
+		printf("%d\n", myArray[i]);
+	}
 	return 0;
 }
