e324b9d583b9a7ad7549f94c2ef644d8d194b0d7
Andrei <drei3000@gmail.com>
2016-11-21 21:10:11 -0700

    quicksort in main back up

--- quicksort.c
+++ quicksort.c
@@ -1,13 +1,47 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 // Quicksort function.
+
+int midPoint(int left, int right) {
+	return (left+right)/2;
+}
+
+int quickSort(int array[], int left, int right) {
+	
+}
+
 int main(int argc, char *argv[]) {
-	int i;
-	int myArray[8] = {3,7,4,9,10,2,5,7};
-
-
-	for (i = 0; i<sizeof(myArray);i++) {
-		printf("%d\n", myArray[i]);
+	int i, temp;
+	int myArray[7] = {3,4,9,6,2,5,7};
+	for (i = 0; i<(sizeof(myArray)/sizeof(int));i++) {
+		printf("%i ", myArray[i]);
 	}
+	printf("\n");
+	int left = 0;
+	int right = sizeof(myArray)/sizeof(int)-1;
+	int pivot = round(midPoint(0, sizeof(myArray)/sizeof(int)));
+	while (left <= right) {
+		while (myArray[left] < myArray[pivot]) {
+			left++;
+		}
+		while (myArray[right] > pivot) {
+			right--;
+		}
+		if (left <= right) {
+			temp = myArray[left];
+			myArray[left] = myArray[right];
+			myArray[right] = temp;
+			left++;
+			right--;
+		}
+	}
+
+	// while (pivot != sizeof(myArray)/sizeof(int)) {}
+	for (i = 0; i<(sizeof(myArray)/sizeof(int));i++) {
+		printf("%i ", myArray[i]);
+	}
+	printf("\n");
 	return 0;
 }
+
