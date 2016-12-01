407f09af9f71d8980b25da12c66713ffe4311f5f
Andrei <drei3000@gmail.com>
2016-12-01 16:36:24 -0700

    working on inplace quicksort

--- quicksort.c
+++ quicksort.c
@@ -35,6 +35,83 @@ void quickSort(int array[], int left, int right) {
 	}
 }
 
+int sizeofArray(array[]) {
+	return sizeof(array)/sizeof(int);
+}
+
+void swapValues(int indexA, int indexB) {
+	int swapTemp = get(indexA);
+	//get(indexA) = get(indexB);
+	put(indexA, get(indexB));
+	put(indexB, swapTemp);
+}
+
+void swap(int* first, int* second) {
+	int temp = *first;
+	*first = *second;
+	*second = temp
+}
+
+int partition(array[], int left, int right) {
+	int pivot = array[left];
+	while (1) {
+		tempLeft++;		
+		while (array[left] > pivot) {
+			left++;	
+		}
+		while (array[right] > pivot) {
+			right--;
+		}
+		if (left <= right) {
+			temp = array[left];
+			array[left] = array[right];
+			array[right] = temp;
+		} else {
+			return right;
+		}
+}
+
+void inplaceQuicksort(int array[], int start, int end) {
+	if (start>end) {
+		return;
+	}
+
+
+
+
+
+	int left, right, pivot;
+	if (end < start+1) {
+		pivot = array[start];
+		left = start+1;
+		right = end;
+		while (left > right) {
+			if (array[left] < pivot) {
+				left++
+			} else {
+				swap(&array[left], &array[right]); 
+			}
+		}
+
+	}
+
+
+	int pivot = array[sizeofArray(array)-1];
+
+	// int insertIndex, sortIndex, insertElement;
+	// for (insertIndex=2; insertIndex<sizeofArray(array);insertIndex++) {
+	// 	insertElement = array[insertElement];
+	// 	sortIndex = insertIndex-1;
+
+	// 	while (insertElement < array[sortIndex] && sortIndex > 0) {
+	// 		array[sortIndex+1] = array[sortIndex];
+	// 		sortIndex--;
+	// 	}
+
+	// 	array[sortIndex+1] = insertElement;
+	// }
+}
+
 int main(int argc, char *argv[]) {
 
 	printf("This is quicksort\n");
@@ -44,7 +121,7 @@ int main(int argc, char *argv[]) {
 		printf("%i ", myArray[i]);
 	}
 	printf("\n");
-	quickSort(myArray, 0, 6);
+	inplaceQuicksort(myArray);
 
 	// while (pivot != sizeof(myArray)/sizeof(int)) {}
 	for (i = 0; i<(sizeof(myArray)/sizeof(int));i++) {
