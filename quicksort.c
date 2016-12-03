bcb0c3c0bd79ae21289fc37e5e5a443e5e15d8f0
Andrei <drei3000@gmail.com>
2016-12-03 14:14:55 -0700

    finished get/puts

--- quicksort.c
+++ quicksort.c
@@ -1,37 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include "simulator.h"
 // Quicksort function.
 // followed the code here: http://alienryderflex.com/quicksort/
 
-void quicksort(int arr[], int lenArr) {
-
+void quicksort(int lenArr) {
 	int piv, begin[100], end[100], i, j, left, right;
-	begin[0] = 0;
 	
+	begin[0] = 0;
 	end[0] = lenArr;
 
 	while (i >= 0) {
-		printf("%i\n", i);
 		left = begin[i];
 		right = end[i]-1;
 		if (left < right) {
-			piv = arr[left];
+			put(SIZE, get(left)); //piv = arr[left]
 			while (left < right) {
-				while (arr[right]>=piv && left < right) {
+				// while (arr[right]>=piv && left < right) {
+				while (get(right) >= get(SIZE) && left < right) {
 					right--;
 				}
 				if (left < right) {
-					arr[left++] = arr[right];
+					put(left, get(right));
+					left++;
+					// arr[left++] = arr[right];
 				}
-				while (arr[left] <= piv && left < right) {
+				while (get(left) <= get(SIZE) && left < right) {
+				// while (arr[left] <= piv && left < right) {
 					left++;
 				}
 				if (left < right) {
-					arr[right--] = arr[left];
+					put(right, get(left));
+					right--;
+					// arr[right--] = arr[left];
 				}
 			}
-			arr[left] = piv;
+			put(left, get(SIZE));
+			// arr[left] = piv;
 			begin[i+1] = left + 1;
 			end[i+1] = end[i];
 			end[i++] = left;
@@ -43,19 +49,35 @@ void quicksort(int arr[], int lenArr) {
 
 int main(int argc, char *argv[]) {
 
-	printf("This is quicksort\n");
-	int i;
-	int myArray[] = {3,4,13,0,9,3,4,6,2,5,11,7,1,20,14};
-	for (i = 0; i<(sizeof(myArray)/sizeof(int));i++) {
-		printf("%i ", myArray[i]);
-	}
-	printf("\n");
-	quicksort(myArray, (sizeof(myArray)/sizeof(int)));
+	init(page_size, window_size);
 
-	// while (pivot != sizeof(myArray)/sizeof(int)) {}
-	for (i = 0; i<(sizeof(myArray)/sizeof(int));i++) {
-		printf("%i ", myArray[i]);
+	int i;
+
+	printf("This is quicksort\n");
+	// put(0, 9);
+	// put(1, 8);
+	// put(2, 6);
+	// put(3, 7);
+	// put(4, 5);
+	// put(5, 4);
+	// put(6, 2);
+	// put(7, 3);
+	// put(8, 1);
+	// put(9, 1);
+	
+	for (i = 0; i<10000; i++) {
+		put(i, lrand48()/10000000);
+		printf("%i ", get(i));
 	}
 	printf("\n");
+
+	quicksort(10000);
+
+	printf("\n");
+	done();
+
+	for (i = 0; i<10000; i++) {
+		printf("%i ", get(i));
+	}
 	return 0;
 }
